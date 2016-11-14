57dbcb680c863c681890ecb685558652886a8835
AdityaGupta1 <adityag@gmail.com>
2016-11-14 09:37:37 -0800

    something weird happened

--- gupta_aditya/loot_collector/_JS/loot_collector.js
+++ gupta_aditya/loot_collector/_JS/loot_collector.js
@@ -1,16 +1,14 @@
 // initialize game
-var game = new Phaser.Game(1000, 600, Phaser.AUTO, 'phaser-example', {
+var game = new Phaser.Game(1000, 600, Phaser.AUTO, 'loot-collector', {
     preload: preload,
     create: create,
     update: update
 });
 
 // sprites
-var sprites = [];
 var player;
 var t1_bullet;
 var playerBullets;
-var enemyBullets;
 var enemies;
 
 // wasd keys
@@ -21,28 +19,22 @@ var dKey;
 
 // player stats
 var playerStats = {
-    "maxLife": 500,
-    "life": 500,
-    "attack": 50,
-    "defense": 10,
-    "speed": 40,
-    "dexterity": 40
+    'maxLife': 500,
+    'life': 500,
+    'attack': 50,
+    'defense': 10,
+    'speed': 40,
+    'dexterity': 50
 };
-
-// // player weapon stats
-// var weaponStats = {
-//     "damage": 50,
-//     "speed": 300,
-//     "lifetime": 750
-// };
 
 // player firing
 var fireRate = 0;
 var nextFire = 0;
 
-// health regen
+// health
 var nextRegen = 0;
 var regenRate = 100;
+var playerHealthBar;
 
 // enemy bullets
 var enemyBulletList = [];
@@ -63,10 +55,6 @@ function changeStat(stat, change) {
     setStat(stat, getStat(stat) + change);
 }
 
-// function getWeaponStat(stat) {
-//     return weaponStats[stat];
-// }
-
 function preload() {
     // player
     game.load.image('wizard', '_img/player/wizard.png');
@@ -82,27 +70,26 @@ function preload() {
 }
 
 function create() {
+    // debug plugin (for development purposes)
     game.add.plugin(Phaser.Plugin.Debug);
 
     game.stage.backgroundColor = '#c6c6c6';
 
+    // all enemies will be part of this group
     enemies = game.add.group();
 
     player = game.add.sprite(500, 300, 'wizard');
     player.bringToTop();
-    sprites.push(player);
+    game.physics.enable(player, Phaser.Physics.ARCADE);
 
     playerBullets = game.add.group();
     playerBullets.enableBody = true;
     playerBullets.physicsBodyType = Phaser.Physics.ARCADE;
 
+    // 100 bullet pool
     playerBullets.createMultiple(100, 't1_bullet');
     playerBullets.setAll('checkWorldBounds', true);
     playerBullets.setAll('outOfBoundsKill', true);
-
-    for (var i = 0; i < sprites.length; i++) {
-        game.physics.enable(sprites[i], Phaser.Physics.ARCADE);
-    }
 
     // bind keys
     wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
@@ -110,27 +97,52 @@ function create() {
     aKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
     dKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
 
-    fireRate = getStat("dexterity") * 10;
+    fireRate = getStat('dexterity') * 10;
 
-    var smallDemonBullets = createEnemyBulletGroup("small_demon_bullet");
-    enemies.add(new Enemy(500, 300, 'small_demon', 500, 'random', 300, smallDemonBullets, 5, 50, 1000));
-    enemyBulletList.push(smallDemonBullets);
+    var smallDemonBullets = createEnemyBulletGroup('small_demon_bullet');
+    enemies.add(new Enemy(500, 300, 'small_demon', 500, 'random', 300, smallDemonBullets, 5, 50, 1000, 10));
 
     player.body.collideWorldBounds = true;
+
+    var playerHealthBarConfig = {
+        width: 200,
+        height: 20,
+        x: 110,
+        y: 20,
+        bg: {
+            color: '#800000'
+        },
+        bar: {
+            color: '#FF0000'
+        },
+        animationDuration: 50,
+        flipped: false
+    };
+
+    playerHealthBar = new HealthBar(game, playerHealthBarConfig)
 }
 
 // called when an enemy bullet hits the player
 function playerDamageHandler(player, enemyBullet) {
     enemyBullet.kill();
-    changeStat("life", -enemyBullet.damage);
+    var damage = enemyBullet.damage;
+    var defense = getStat('defense');
+    // defense subtracts from damage, but the enemy bullet has to deal at least 10% of its original damage
+    changeStat('life', (damage - defense) < (damage * 0.1) ? ((-damage) * 0.1) : ((-damage) + defense));
+    if (getStat('life') < 0) {
+        player.kill();
+    }
 }
 
 function update() {
+    // check if player is touching enemy bullets
     for (var i = 0; i < enemyBulletList.length; i++) {
         game.physics.arcade.overlap(player, enemyBulletList[i], playerDamageHandler, null, this);
     }
 
-    console.log(getStat("life"));
+    game.physics.arcade.overlap(enemies, playerBullets, enemyDamageHandler, null, this);
+
+    playerHealthBar.setPercent((getStat('life') / getStat('maxLife')) * 100);
 
     eightWayMovement();
     playerShoot();
